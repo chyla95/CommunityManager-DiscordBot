0a269ee644f42329e2488b37d4592a3dc6bb0ae5
chyla95 <43494554+chyla95@users.noreply.github.com>
2020-06-26 19:13:33 +0200

    1st commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '.test') {
+    msg.reply('Test message.');
+  }
+});
+
+client.login('NzI2MDcwMjIwNTczMjQ1NDYw.XvX7xg.XVOp99U-i_K_XYFgIv6wyH5mWUA');
