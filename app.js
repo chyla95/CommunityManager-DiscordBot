6bafb9359c546889775814284754c97dea124801
chyla95 <43494554+chyla95@users.noreply.github.com>
2020-06-26 19:57:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, MessageEmbed } = require('discord.js');
+
+const client = new Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -13,7 +14,19 @@ client.on('message', msg => {
 
 client.on('message', msg => {
   if (msg.content === '.ingoz') {
-    msg.send('Ingoz is gay lol.');
+    
+    const embed = new MessageEmbed()
+      // Set the title of the field
+      .setTitle('Ingoz')
+      // Set the color of the embed
+      .setColor(0xF340B9)
+      // Set the main content of the embed
+      .setDescription('Ingoz is very gay. :sademote:');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+
+
+
   }
 });
 
