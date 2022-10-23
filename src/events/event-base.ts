eda97a95ebeea54219fe2e46630937ca087797fc
chyla95 <chyla95@gmail.com>
2022-10-23 07:56:11 +0200

    Update Event Structure

--- src/events/event-base.ts
+++ src/events/event-base.ts
@@ -4,5 +4,5 @@ export abstract class EventBase {
   abstract readonly triggerEvent: Events;
   abstract readonly description: string;
 
-  abstract execute(arg: any): Promise<void>;
+  abstract execute(...arg: any[]): Promise<void>;
 }
--- src/events/sample-event.ts
+++ src/events/sample-event.ts
@@ -5,7 +5,7 @@ export default class SampleEvent extends EventBase {
   readonly triggerEvent = Events.MessageCreate;
   readonly description = "Sample event, used as a placeholder.";
 
-  async execute(arg: Message) {
-    if (arg.content == "sample") console.log(`Sample Event Triggered. Args: ${arg}.`);
+  async execute(message: Message) {
+    if (message.content == "sample") console.log(`Sample Event Triggered. Args: ${message}.`);
   }
 }
